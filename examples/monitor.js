06b9f769781ed98e2f50ad1fe05fc2514b7b27ba
Andrey Sidorov <sidorares@yandex.ru>
2012-08-24 15:23:31 +1000

    use session bus from env

---
+++ examples/monitor.js
@@ -0,0 +1,43 @@
+var dbus = require('../index.js');
+//var conn = dbus({socket: '/var/run/dbus/system_bus_socket'});
+var conn = dbus();
+
+conn.message({
+   type: 1,
+   serial: 1,
+   path: '/org/freedesctop/DBus',
+   destination: 'org.freedesktop.DBus',
+   interface: 'org.freedesktop.DBus',
+   member: 'Hello',
+});
+
+conn.message({
+   path: '/org/freedesctop/DBus',
+   destination: 'org.freedesktop.DBus',
+   interface: 'org.freedesktop.DBus',
+   member: 'AddMatch',
+   signature: 's',
+   body: ["type='signal'"]
+});
+
+conn.message({
+   path: '/org/freedesctop/DBus',
+   destination: 'org.freedesktop.DBus',
+   interface: 'org.freedesktop.DBus',
+   member: 'AddMatch',
+   signature: 's',
+   body: ["type='method_call'"]
+});
+
+conn.message({
+   path: '/org/freedesctop/DBus',
+   destination: 'org.freedesktop.DBus',
+   interface: 'org.freedesktop.DBus',
+   member: 'AddMatch',
+   signature: 's',
+   body: ["type='method_return'"]
+});
+
+conn.on('message', function(msg) {
+   console.log(msg); //TODO: dbus-monitor pretty-print
+});
