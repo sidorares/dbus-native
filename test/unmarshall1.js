cc129414061a6b667480b744db1840326f8a63f6
Andrey Sidorov <sidorares@yandex.ru>
2012-12-30 16:17:18 +1100

    use absolute path to fixtures

--- test/unmarshall1.js
+++ test/unmarshall1.js
@@ -1,11 +1,12 @@
 var hexy = require('../lib/hexy').hexy;
 var unmarshall = require('../lib/unmarshall');
 var signature = "a(ssasib)";
+var common = require('./common');
 
-var data = require('fs').readFileSync('./fixtures/problembody.bin');
+var data = require('fs').readFileSync(common.fixturesDir + '/problembody.bin');
 var bufstream = require('binary').parse(data);
 
-unmarshall.call(bufstream, signature, function(err, result) {
+unmarshall.call(bufstream, signature, 0, function(err, result) {
     console.log(result);
     console.error(hexy(data, {prefix: '===='}));
 });
