32b6670dd51d3a25de8476ad06c15e5e4c00532e
Andrey Sidorov <sidorares@yandex.ru>
2013-09-13 11:13:35 +1000

    serialize d type as double, not float #25

--- examples/service/server2.js
+++ examples/service/server2.js
@@ -1,5 +1,9 @@
 var dbus = require('../../index.js');
 
+// command line to test:
+// dbus-send --print-reply --type=method_call --dest='some.name' '/com/github/sidorares/1' com.example.service.respondWithDouble string:'test123'
+// dbus-send --print-reply --type=method_call --dest='some.name' '/com/github/sidorares/1' com.example.service.timesTwo double:123.4567
+
 //var addrx11 = require('../../lib/address-x11');
 //addrx11(function(err, address) {
 //var bus = dbus.sessionBus({busAddress: address});
@@ -11,7 +15,9 @@ bus.requestName(name, 0);
 var exampleIface = {
     name: 'com.example.service',
     methods: {
-        doStuff: ['s', 's']
+        doStuff: ['s', 's'],
+        timesTwo: ['d', 'd'],
+        respondWithDouble: ['s', 'd']
     },
     signals: {
         testsignal: [ 'us', 'name1', 'name2' ]
@@ -22,6 +28,14 @@ var exampleIface = {
 };
 
 var example = {
+    respondWithDouble: function(s) {
+        console.log('Received "' + s + "'");
+        return 3.14159;
+    },
+    timesTwo: function(d) {
+	console.log(d);
+        return d*2;	
+    },
     doStuff: function(s) {
         return 'Received "' + s + '" - this is a reply'; 
     },
@@ -33,8 +47,10 @@ var example = {
 bus.exportInterface(example, '/com/github/sidorares/1', exampleIface);
 bus.exportInterface(example, '/com/github/sidorares/2', exampleIface);
 
-setInterval( function() {
-    example.emit('testsignal', Date.now(), 'param2');
-}, 1000);
+//setInterval( function() {
+//    example.emit('testsignal', Date.now(), 'param2');
+//}, 1000);
 
 //});
+//
+
