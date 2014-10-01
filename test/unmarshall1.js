4cabb217fc32f830ffc354ac560039cbe983a672
Andrey Sidorov <sidorares@yandex.ru>
2014-10-02 09:56:12 +1000

    remove old test

--- test/unmarshall1.js
+++
@@ -1,7 +0,0 @@
-var hexy = require('../lib/hexy').hexy;
-var unmarshall = require('../lib/unmarshall');
-var signature = "a(ssasib)";
-//var data = require('fs').readFileSync(__dirname + '/fixtures/problembody.bin');
-//console.log(hexy(data));
-//var obj = unmarshall(data, signature);
-//console.log(obj);
