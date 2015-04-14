a794b2560b9ac0bbfd3716165c7f01b932f770a7
Andrey Sidorov <sidorares@yandex.com>
2015-04-14 10:27:52 +1000

    remove server to service

---
+++ test/functional/service.js
@@ -0,0 +1,72 @@
+/**
+ * Test basic dbus service
+ */
+var dbus    = require("../index.js");
+var assert  = require('assert');
+
+// test locally atm, requires running dbus daemon.
+// TODO: start daemon on travis or mock daemon
+if(process.env.CI)
+    return;
+
+describe("server", function() {
+
+    var testPath = "/com/nodedbustest/service";
+    var testName = "com.nodedbustest.service";
+    var testInterfaceName = "com.nodedbustest.service";
+    var bus = dbus.sessionBus();
+    var testInterface = {
+        name: testInterfaceName,
+        methods: {
+            multiplyByTwo: ['y', 'y'],
+            addTwoNumbers: ['yy', 'y']
+        }
+    };
+
+    var testImplementation = {
+        multiplyByTwo: function(a) {
+            return 2 * a;
+        },
+        addTwoNumbers: function(a, b) {
+            return a + b;
+        }
+    };
+
+    before(launchService(bus, name, path, interfaceName, implementation)  {
+        bus.requestName(testName, 0);
+        bus.exportInterface(testImplementation, testPath, testInterfaceName);
+    }
+
+    it("serves a basic service", function(done) {
+        //don't run if not on local machine
+        bus.invoke({
+            path: testPath,
+            destination: testName,
+            interface: testInterfaceName,
+            member: 'multiplyByTwo',
+            signature: 'y',
+            body: [2]
+        }, function(err, result) {
+            assert.ifError(err);
+            assert.equal(result, 4);
+            done();
+        });
+    });
+
+    it("serves a basic service, uses different method on same service", function(done) {
+        var a = 2;
+        var b = 5;
+        bus.invoke({
+            path: testPath,
+            destination: testName,
+            interface: testInterfaceName,
+            member: 'addTwoNumbers',
+            signature: 'yy',
+            body: [a, b]
+        }, function(err, result) {
+            assert.ifError(err);
+            assert.equal(result, a + b);
+            done();
+        });
+    });
+});
