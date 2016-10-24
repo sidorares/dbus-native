875464e46a5f13e9b2f0f162e8cfe25490552fab
Nicolas SCHOEMAEKER <nicolas.schoemaeker@partnering.fr>
2016-10-24 15:11:15 +0200

    Update examples for the new API

--- examples/basic-client-new-API.js
+++ examples/basic-client-new-API.js
@@ -8,42 +8,61 @@ const Promise = require ('bluebird')
 	This example file's purpose is to demonstrate how to make a simple, basic with the new API (DBus proxy).
 	In order to do that, we connect to the session bus, create a DBus Service to target a distant DBus service.
 	We demonstrate how to make various method calls, how to get and set property values and how to listen for signals.
-	Make sure to start the basic service with `node basic-server.`js` before.
+	Make sure to start the basic service with `node basic-server-new-API.`js` before.
+
+	For information, the spirit of the binding with the DBusProxy is this one: you create a DBusProxy which is an object
+	that represents the remote DBus service.
+	This object is as close as possible to the service, so that you can abstract your work: the fact that you are
+	working with a remote DBus service should be transparent to you, so:
+	- to make a method call, simply call a function on the proxy you have
+	- properties are getter/setters, so simply get or set the proeprty like a normal Javascript property
+	- interfaces are EventEmitter, and the binding takes care of matching the DBus signal with the Javascript signals,
+	  so to listen to a DBus signal, you simply listen to the Proxy (EventEmitter) signal
+
+	It is _really_ all that simply. Obviously, this all of this is asynchronous, everything that was just described is
+	implemented as promised.
 */
 
+// Firt we need to connect to the bus (session or system0)
 const sessionBus = dbus.sessionBus()
 
 // Name of the service we will make a proxy for
 const targetService = 'com.dbus.native.basic.service'
 
-sessionBus.getService2 (targetService) // actual call to create a proxy for the service
-.then( (service) => { // `service` is a DBusService2
+
+/*
+	This is the call that actually makes the magic happen and create the DBusProxy
+*/
+sessionBus.getService2 (targetService)
+.then( (service) => {
 	/*
-		Attributes of the DBusService are the children object (DBus sense)
+		Once this call returns, it has created a DBusProxy for us, with everythign that was described above available.
+
+		Attributes of the DBusProxy are the children object (DBusObject)
 		So here 'com' represents the '/com' object, which just has the 'dbus' object, so 'service.com.dbus' is
 		the object '/com/dbus' etc.
-		To save typing we save the object we are interested in in a 'obj' variable.
+		To save typing we save the object we are interested in an 'obj' variable.
 		When we reach an object that has interfaces, they are string properties whose name is the full string name of
 		the interface.
 	*/
-	let obj = service.com.dbus.native.basic.service
+	let obj = service.com.dbus.native.basic.service // That's traversing the hierarchy, DBus-style
 
-	// Switch to 'false' not to display the final service
-	if (true) {
+	// Switch to 'true' to display the service completely
+	if (false) {
 		console.log ('Got service:')
 		console.log (inspect (service, {colors: true, depth: 6}))
 	}
 
-	// Make a function call, just to check. Make it random to see that several functions call actually work
+	// Make a function call, just to check. Make it random to see that several functions call actually work (see below)
 	let fn = Math.round (Math.random() * 100) >= 50
 		? 'SayHello'
 		: 'GiveTime'
 
 	// Accessing a property is calling the function whose name is the property's, with no argument. Promise-based.
-	obj["com.dbus.native.basic.service"].Flag()
+	obj["com.dbus.native.basic.service"].Flag() // That's a normal getter (but promisified)
 	.then( (flagValue) => {
 		/*
-			We actually got the value here.
+			We actually got the value here whe nthe getter returns.
 			Though this is promised-based, the value is returned immediately, because the property value is stored
 			locally in the DBusInterface2. It is updated automatically when 'PropertiesChanged' is received.
 		*/
@@ -59,7 +78,7 @@ sessionBus.getService2 (targetService) // actual call to create a proxy for the 
 			'PropertiesChanged' signal to update its property's value.
 			This is done so that the proxy doesn't get update if the DBus SET call fails for some reason.
 		*/
-		return obj["com.dbus.native.basic.service"].Flag(!flagValue)
+		return obj["com.dbus.native.basic.service"].Flag(!flagValue) // simple setter syntaxt (but promisified)
 	})
 	.then( () => {
 		// Here we query for the property value again, to show that the binding does work
@@ -75,15 +94,28 @@ sessionBus.getService2 (targetService) // actual call to create a proxy for the 
 	})
 
 	/*
-		An interface can have signals too, so here is how we listen to a signal from the interface
+		Here we show how simple it is to listen to a signal: it's simply an EventEmitter!
+		Let the binding worry about translating DBus signals into EventEmitter signals!
 	*/
 	obj["com.dbus.native.basic.service"].on ('Rand', nb => {
 		console.log ('Rand gave out: ' + nb)
 	})
 
+	/*
+		Same thing, just to show that complex, container types get marshalled correctly
+	*/
+	obj["com.dbus.native.basic.service"].on ('TestSig', arg => {
+		console.log ('arg: ' + inspect (arg))
+		// console.log ('TestSig str: ' + str)
+		// console.log ('TestSig bool: ' + bool)
+	})
+
 	// The small wait here is just made so that the output doesn't all come in one.
 	setTimeout (() => {
-		// Here is how to make a method call on the proxy, we call Capitalize
+		/*
+			This demonstrates how to make method clal: simply call the interface's method.
+			Obf course this is asynchronous so it's promised-based, but this is the only real gotcha!
+		*/
 		obj["com.dbus.native.basic.service"].Capitalize ('lower String')
 		.then( (ret) => {
 			console.log ('Capitalized returned: ' + ret)
--- examples/basic-service-new-API.js
+++ examples/basic-service-new-API.js
@@ -16,6 +16,12 @@ const DBusInterface = DBusInterfaceLibs.DBusInterface2
 
 const DBusObject    = DBusObjectLibs.DBusObject2
 
+/*
+	This file's purpose is to demonstrate how to use the new API to define a simple DBus service.
+	Note that this file is rather big because it is very much commented and each line is explained.
+	We define a number of functions that do nothing very interesting, but they show how to use almost all DBus types.
+*/
+
 // The name we want to expose our DBus service on
 const serviceName   = 'com.dbus.native.basic.service'
 
@@ -39,7 +45,11 @@ if (!sessionBus) {
 	throw new Error ('Could not connect to the DBus session bus.')
 }
 
-// Create our interface
+/*
+	We create our interface.
+	In the new API, an interface is the object that has the methods, properteis and signals that can be called, get/set
+	or emitted.
+*/
 let iface = new DBusInterface (interfaceName)
 
 /*
@@ -50,11 +60,23 @@ let iface = new DBusInterface (interfaceName)
 |_| |_| |_|\___|\__|_| |_|\___/ \__,_|___/
 */
 
-// Create a function 'Capitalize' that we want to export on the bus
+/*
+	Here is the part where we define methods.
+	Defining a method for DBus is done in two steps:
+	1) define a (Javascript) method in the interface object. This is a normal Javascript method. Is synchronous, you can
+	   simply return the value(s). In case of error, throw a (real) Javascript Error.
+	   If asynchronous, use and return a promise, in case of failing, throw a (real) Javascript error.
+	2) "annotate" the function (this step is required because DBus needs to know the function's argument's types.). This
+	   step is boilerplate (because we need to manaully specify some code, and in theory, nothing prevents us from
+       annotating with types that do not correspond to the function's definition) but we have no other choice as
+	   Javascript is not typed at all and DBus is strongly typed.
+*/
+
+// Create a function 'Capitalize' that we want to export on the bus: this is a normal Javascript function
 iface.Capitalize = function (str) {
 	console.log ('Capitalized called with ' + str)
 
-	// Here is how to return a single type (a string here)
+	// Here is how to return a single type (a string here): it's just returning the value, as usual
 	return str.toUpperCase()
 }
 /*
@@ -63,7 +85,11 @@ iface.Capitalize = function (str) {
 	the same name than the function on the interface
 */
 DBusMethod (iface, 'Capitalize', {
-	// The keys will be the arguments's name, NO SPACE allowed.
+	/*
+		The keys will be the arguments's name, NO SPACE allowed.
+		When there are only one argument (like here for 'input' and for 'output'), we can define a single object.
+		But when there are several, 'input' nad?or 'output' should be an array of those object (see below)
+	*/
 	input: {initial_string: t.DBUS_STRING},
 	output: {capitalized_string: t.DBUS_STRING}
 })
@@ -72,7 +98,7 @@ DBusMethod (iface, 'Capitalize', {
 iface.SayHello = function () {
 	console.log ('SayHello called')
 
-	return 'Hello, world!'
+	return 'Hello, world!' // return a string
 }
 DBusMethod (iface, 'SayHello', {
 	input: {}, // where there is no input (or output) parameter, just assign the empty object {}
@@ -84,7 +110,7 @@ iface.AddNumbers = function (n1, n2) {
 	console.log ('n1 = ' + n1)
 	console.log ('n2 = ' + n2)
 
-	// throw new TypeError ('test')
+	// as usual, returning a single, non-container type is straightforward.
 	return n1 + n2
 }
 DBusMethod (iface, 'AddNumbers', {
@@ -96,7 +122,9 @@ DBusMethod (iface, 'AddNumbers', {
 	output: {sum: t.DBUS_INT32}
 })
 
-// Here is how to return a DBus STRUCT
+/*
+	This function is here to illustrate how to use DBus STRUCT types.
+*/
 iface.GetStruct = function() {
 	// A DBus STRUCT is represented an an object whose keys are integers (in the order of the struct)
 	let ret = {
@@ -109,34 +137,109 @@ iface.GetStruct = function() {
 }
 DBusMethod (iface, 'GetStruct', {
 	input: {},
-	output: {struct: t.DBUS_STRUCT (t.DBUS_STRING, t.DBUS_INT32, t.DBUS_BOOL)} // the syntaxt is straightforward
+	/*
+		The syntax is pretty straightforward: for all DBus container types (i.e. ARRAY, DICT and STRUCT), the annotation
+		is a function, which takes the contained values' types.
+		For a structure, they are the types of the elements, in the order in which they appear in the struct
+	*/
+	output: {struct: t.DBUS_STRUCT (t.DBUS_STRING, t.DBUS_INT32, t.DBUS_BOOL)}
 })
 
-// Here is how to return a DBus DICT
+/*
+	This function is here to illustrate how to use DBus DICT types.
+*/
 iface.GetDict = function () {
+	/*
+		As you can see, a DBus DICT matches really well with Javascript's Map.
+		The keys must be of the first type specified in the signature, and the values of the second type.
+		This is pretty straightforward.
+	*/
 	let m = new Map()
 
-	m.set ('foo', 33)
+	m.set ('foo', 33) // here is how to associate the key 'foo' with the value 33
 	m.set ('bar', 1089)
 	m.set ('foobar', 0)
 
+	// To return a DBus DICT, simply return a Javascript Map
 	return m
 }
 DBusMethod (iface, 'GetDict', {
 	input: {},
+	/*
+		To annotate a DBus DICT, this is easy, the first type is the type of the keys, and the second is the value's
+	*/
 	output: [
-		{dict: t.DBUS_DICT (t.DBUS_STRING, t.DBUS_INT16)} // First type is key, second type is value
+		{dict: t.DBUS_DICT (t.DBUS_STRING, t.DBUS_INT16)}
 	]
 })
 
-// Here is how to return anarray of values
+/*
+	This function is here to illustrate how to use DBus ARRAY types.
+*/
 iface.GetArray = function () {
+	// A DBus array matches is represented very simply by a Javascript array
 	return ['foo', 'bar', 'quux']
 }
 DBusMethod (iface, 'GetArray', {
 	input: {},
+	// To annotate an array, simple use DBUS_ARRAY() with one argument: the type of the array's elements
 	output: {array: t.DBUS_ARRAY (t.DBUS_STRING)}
 })
+
+/*
+	As of now, we have presented the major DBus types supported by dbus-native.
+	But most of them were supported through return type (i.e. functions returned these types)
+	The following functions just demontrate that, of course, it's possible to pass those types as arguments (everything
+    is exactly the same, but the types are in the 'input' field now)
+*/
+
+/*
+	This is a function that takes an array in input (and return the number of items inside)
+*/
+iface.CountElems = (arr) => {
+	return arr.length
+}
+DBusMethod (iface, 'CountElems', {
+	input: [
+		{input_array: t.DBUS_ARRAY (t.DBUS_STRING)} // as said before: this is exactly like for output values
+	],
+	output: [
+		{nb_elems: t.DBUS_UINT16}
+	]
+})
+
+/*
+	This function is similar to the previous one: only it takes DICT (which is a Map in Javascript) and returns the
+	number of elements inside.
+*/
+iface.CountKeys = (map) => {
+	console.log ('CountKeys received:\n' + inspect (map))
+	return map.size
+}
+DBusMethod (iface, 'CountKeys', {
+	input: [
+		{input_struct: t.DBUS_DICT (t.DBUS_STRING, t.DBUS_INT16)}
+	],
+	output: [
+		{nb_keys: t.DBUS_UINT16}
+	]
+})
+
+/*
+	Again, same kind of functions, but it takes a STRUCT,, it also counts the number of types inside.
+*/
+iface.CountTypes = (struct) => {
+	console.log ('CountTypes received: ' + inspect (struct))
+	return Object.keys(struct).filter(k => Number.isInteger (Math.floor (k))).length
+}
+DBusMethod (iface, 'CountTypes', {
+	input: {input_struct: t.DBUS_STRUCT (t.DBUS_STRING, t.DBUS_INT32, t.DBUS_BOOL)},
+	output: {nb_types: t.DBUS_UINT32}
+})
+
+/*
+	Now that you have seen alsmot all of what's possible, don't hesitate to experiment and mix the types
+*/
 
 /*
                                  _   _
@@ -145,6 +248,16 @@ DBusMethod (iface, 'GetArray', {
 | |_) | | | (_) | |_) |  __/ |  | |_| |  __/\__ \
 | .__/|_|  \___/| .__/ \___|_|   \__|_|\___||___/
 |_|             |_|
+*/
+
+/*
+	The following sectios shows how to define DBus properties.
+	This follows the same spirit as for functions (methods).
+	A DBus property is simply a Javascriptn property on the interface object.
+	1) define the property by standard Javascript assignment
+	2) "annotate" the property to tell DBus its type and so that the binding can make a getter/setter out of this
+	   property.
+	   The access mode is driven by the name of the key in the object passed to the annotation function (see example)
 */
 
 // To define a property, you just define the Javascript property...
@@ -157,36 +270,129 @@ The key of the last object can either be:
  - 'readwrite' for readwrite access
 */
 DBusProperty (iface, 'Flag', {
-	read: t.DBUS_BOOL
+	read: t.DBUS_BOOL // readonly access
 })
 
+// This illustrates how to define a string property
 iface.StringProp = 'initial string'
 DBusProperty (iface, 'StringProp', {
-	readwrite: t.DBUS_STRING
+	readwrite: t.DBUS_STRING // readwrite access
 })
 
+/*
+     _                   _
+ ___(_) __ _ _ __   __ _| |___
+/ __| |/ _` | '_ \ / _` | / __|
+\__ \ | (_| | | | | (_| | \__ \
+|___/_|\__, |_| |_|\__,_|_|___/
+       |___/
+*/
+
+/*
+	The following sections shows how to define signals.
+	Again, it follows the same logic. It is exactly like defining methods, but there should only be an 'output' field.
+	Note that since interfaces are EventEmitter, there is no need to 'register' an event to it beforehand,
+	So the only necessary step is to "annotate" the signal, for DBus to know the types.
+	Then when it's time to emit the signal, simply use `iface.emit('<signal-name>', <signal-arg-1>, <signal-arg-2>, etc.)`
+*/
+
+/*
+	This defines a signal named 'Rand' which has one argument: an int32
+*/
 DBusSignal (iface, 'Rand', {
-	random_number: t.DBUS_INT32
+	output: [
+		{random_number: t.DBUS_INT32},
+	],
 })
 
+/*
+	This defines a signal 'TestSig' which emits a STRUCT
+*/
+DBusSignal (iface, 'TestSig', {
+	output: [
+		{sig_param: t.DBUS_STRUCT (t.DBUS_STRING, t.DBUS_BOOL)}
+		// {string_arg: t.DBUS_STRING},
+		// {bool_arg: t.DBUS_BOOL}
+	],
+})
 
-// Create an object that implements the interface
+/*
+	Here we use an interval with randomness so that the signals are not fired too regularly.
+	Of course, in real applications, emit signals wherever you want.
+*/
+setInterval( () => {
+	let rand = Math.round (100 * Math.random())
+
+	if (rand >= 50) {
+		let nb = Math.round (100 * Math.random())
+		console.log ('[EMIT RAND] ' + nb)
+
+		/*
+			This is how to really emit the 'Rang' signal: it is emitted EXACTLY like a Javascript's EventEmitter signal
+			Simply pass the argument(s) after the name.
+			It is REALLY easy and comes naturally.
+		*/
+		iface.emit ('Rand', nb)
+	}
+}
+, 2000)
+
+/*
+	Same remark for the randomness.
+	Here we emit the 'TestSig' with a slightly more complex type: a STRUCT (object with integer keys)
+*/
+setInterval( () => {
+	let rand = Math.round (100 * Math.random())
+
+	if (rand >= 50) {
+		console.log ('[EMIT TESTSIG] ')
+
+		// Emitting is simply emitting the EventEmitter signals
+		iface.emit ('TestSig', {
+			0: 'hello, world!',
+			1: false
+		})
+	}
+}
+, 2500)
+
+/*
+	Now you have finished defining your interface's methods, properties and signals.
+	It's time to wrap up everything in the desired object path, create the DBusService object and expose it on the bus.
+*/
+
+/*
+	Create an object that implements the interface
+	You can have several object that implement the same interface: this is the strengh of this approach.
+*/
 let obj = new DBusObject (iface) // add the interface directly on build
 
-// Note that it is also possible to add an interface with `obj.addInterface (iface)`
+// Note that it is also possible to add an interface with `obj.addInterface (iface)` It's really up to you
 
 
-// Create the service
+/*
+	Create the service
+	A service has several objects and each of these object have interfaces
+*/
 let service = new DBusService ()
 
-// Add an child object to the service (note that the path must be relative: no initial '/')
+/*
+	Add an child object to the service (note that the path must be relative: no initial '/')
+	Note that, similarly to object and interfaces, you could directly pass the obj and the relative path to the
+	service's constructor, as such: `let service = new DBusService (obj, 'com/dbus/native/basic/service')`
+*/
 service.addObject (obj, 'com/dbus/native/basic/service')
 
-// Expose the service on the bus so that it's usable by other services
+/*
+	Expose the service on the bus so that it's usable by other services
+	It's at _this moment_ that the service becomes available on the bus.
+*/
 sessionBus.exposeService2 (service, serviceName) // the call is promisified
 .then (() => {
+	// In the '.then()', it means the servie is correclty exposed on the bus
 	console.log ('Service exposed and ready to answer calls, with name \'' + serviceName + '\'')
 })
 .catch( (err) => {
+	// In the `.catch()`, it means there was a problem while trying to expose the service on the bus
 	console.error ('Failed to exposed service on bus: ' + err)
 })
