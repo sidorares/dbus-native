4c1a61c18c9a3074bf8c4866e3d7dad127eb54bb
johnny <johnny@localmomentum.net>
2017-10-06 01:33:12 -0400

    add curlies to all if/else usage

--- examples/p2p/cli.js
+++ examples/p2p/cli.js
@@ -3,8 +3,11 @@ var dbus = require('../../index.js');
 var count = 0;
 conn = dbus.createConnection({ port: 3333, handshake: 'none' });
 conn.on('message', function(msg) {
-  if (msg.serial) msg.serial += 1;
-  else msg.serial = 1;
+  if (msg.serial) {
+    msg.serial += 1;
+  } else {
+    msg.serial = 1;
+  }
   conn.message(msg);
   count++;
 });
